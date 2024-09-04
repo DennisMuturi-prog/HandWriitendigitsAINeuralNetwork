e2e4e52c475c67a4c108ffb2770b9eb7f8744baa
Dennis <deynohmuturi@gmail.com>
2024-09-04 20:35:07 +0300

    chore: Refactor neuralNet.py for improved code structure and readability

--- src/neuralNet.py
+++ src/neuralNet.py
@@ -1,7 +1,6 @@
 from dataLoader import prepareForTraining
 import numpy as np
 import random
-from mnist_loader import load_data_wrapper
 def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z)) 
 def sigmoid_prime(z):
@@ -32,9 +31,6 @@ class NeuralNetwork:
         self.sumGradient_b2=np.zeros([structure[2],1])
         self.sumGradient_w3=np.zeros([structure[3],structure[2]])
         self.sumGradient_b3=np.zeros([structure[3],1])
-
-
-
     
     def feedForward(self,a0):
         self.a0=a0
@@ -55,33 +51,20 @@ class NeuralNetwork:
         gradient_w2=gradient_b2@self.a1.T
         gradient_b1=(((gradient_b2.T@self.w2).T)*sigmoid_prime(self.z1))
         gradient_w1=gradient_b1@self.a0.T
-        self.sumGradient_b3+=gradient_b3
-        self.sumGradient_w3+=gradient_w3
-        self.sumGradient_b2+=gradient_b2
-        self.sumGradient_w2+=gradient_w2
-        self.sumGradient_b1+=gradient_b1
-        self.sumGradient_w1+=gradient_w1
+        self.sumGradient_b3=gradient_b3
+        self.sumGradient_w3=gradient_w3
+        self.sumGradient_b2=gradient_b2
+        self.sumGradient_w2=gradient_w2
+        self.sumGradient_b1=gradient_b1
+        self.sumGradient_w1=gradient_w1
 
     def tuneNetwork(self):
-        average_grad_w1=0.1*(self.sumGradient_w1)
-        average_grad_b1=0.1*(self.sumGradient_b1)
-        average_grad_w2=0.1*(self.sumGradient_w2)
-        average_grad_b2=0.1*(self.sumGradient_b2)
-        average_grad_w3=0.1*(self.sumGradient_w3)
-        average_grad_b3=0.1*(self.sumGradient_b3)
-        # print("average grad w3",average_grad_w3.shape)
-        self.sumGradient_w1=np.zeros([self.structure[1],self.structure[0]])
-        self.sumGradient_b1=np.zeros([self.structure[1],1])
-        self.sumGradient_w2=np.zeros([self.structure[2],self.structure[1]])
-        self.sumGradient_b2=np.zeros([self.structure[2],1])
-        self.sumGradient_w3=np.zeros([self.structure[3],self.structure[2]])
-        self.sumGradient_b3=np.zeros([self.structure[3],1])
-        self.w1+=-3*(average_grad_w1)
-        self.w2+=-3*(average_grad_w2)
-        self.w3+=-3*(average_grad_w3)
-        self.b1+=-3*(average_grad_b1)
-        self.b2+=-3*(average_grad_b2)
-        self.b3+=-3*(average_grad_b3)
+        self.w1+=-0.3*(self.sumGradient_w1)
+        self.w2+=-0.3*(self.sumGradient_w2)
+        self.w3+=-0.3*(self.sumGradient_w3)
+        self.b1+=-3*(np.mean(self.sumGradient_b1,axis=1).reshape(self.structure[1],1))
+        self.b2+=-3*(np.mean(self.sumGradient_b2,axis=1).reshape(self.structure[2],1))
+        self.b3+=-3*(np.mean(self.sumGradient_b3,axis=1).reshape(self.structure[3],1))
     
     def evaluateNetwork(self,startingInput,desiredOutput):
         networkOutput=self.feedForward(startingInput)
@@ -90,8 +73,10 @@ class NeuralNetwork:
             return True
         else:
             return False
-
+        
+#must train with first layer with 784 and last layer because of architecture of our network and with four layers
 myNetwork=NeuralNetwork([784,128,64,10])
+training_data,testingData=prepareForTraining()
 def shuffleTrainingData():
     random.shuffle(training_data)
     return training_data
@@ -102,19 +87,26 @@ def batchTrainingData():
         batchedData.append(shuffledData[i:i+10])
     return batchedData
 
-training_data,testingData=prepareForTraining()
-for i in range(50):
-    training_set=batchTrainingData()
-    for training_batch in training_set:
-        for trainingInput,desiredOutput in training_batch:
-            myNetwork.feedForward(np.array(trainingInput))
-            myNetwork.backwardPropagation(np.array(desiredOutput))
-        myNetwork.tuneNetwork()
-    correctClassification=0
-    for testingInput,desiredTestingOutput in testingData:
-        if myNetwork.evaluateNetwork(np.array(testingInput),np.array(desiredTestingOutput)):
-            correctClassification+=1
-    print("correctClassification:",correctClassification)
+def trainNetwork(epoch):
+    for i in range(epoch):
+        training_set=batchTrainingData()
+        for training_batch in training_set:
+            x_input=[]
+            y_output=[]
+            for trainingInput,desiredOutput in training_batch:
+                x_input.append(trainingInput)
+                y_output.append(desiredOutput)
+            myNetwork.feedForward(np.array(x_input).reshape(10,784).T)
+            myNetwork.backwardPropagation(np.array(y_output).reshape(10,10).T)
+            myNetwork.tuneNetwork()
+        correctClassification=0
+        for testingInput,desiredTestingOutput in testingData:
+            if myNetwork.evaluateNetwork(np.array(testingInput),np.array(desiredTestingOutput)):
+                correctClassification+=1
+        print("correctClassification:",correctClassification)
+
+trainNetwork(30)
+        
 
 
 
